c27e33ac8bd1177968b43aa3e02b94bc4dbe17c1
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-06-10 12:22:05 +0300

    Updates

---
+++ precache-manifest.2920ce92d90e7262174e57f6625c9401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976880a55e782a3a66418920388455d4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dfc51577dcf8f91eb94f",
+    "url": "/Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/Portfolio/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc51577dcf8f91eb94f",
+    "url": "/Portfolio/static/js/main.07b7ffc2.chunk.js"
+  },
+  {
+    "revision": "5ca84006f8232717ccc5",
+    "url": "/Portfolio/static/js/runtime-main.ff77c47e.js"
+  }
+]);
